f7da3ed5935295383a15516c28dba51b37d95b20
KayenneG <166542017+KayenneG@users.noreply.github.com>
2025-01-27 15:08:54 -0800

    Unity Initial

---
+++ Assets/Week One/Scripts/Datatype Practice.cs
@@ -0,0 +1,62 @@
+using System.Runtime.CompilerServices;
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class DatatypePractice : MonoBehaviour
+{
+    //int and float datatypes are both numbers; int are whole float are decimal
+    //char is an independent letter, and string is a full 'sting' or letters (words)
+
+    int santaBanta;
+
+    float total;
+
+    public float dinoMan = 2;
+
+    public float mariririririooooo;
+
+    char beebong = 'B';
+
+    string fullPhrase;
+    string uhhhhh = "UHHHHHHH";
+    string theresMore = "huzzah";
+
+    public string nameIfYouWill;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        santaBanta = 68;
+
+        santaBanta = (santaBanta + 7) * 2 / 3 - 27;
+
+        Debug.Log(santaBanta);
+
+        Debug.Log(dinoMan);
+
+        //both of these do the same thing! Just different ways of doing it. 
+        dinoMan = dinoMan - 2;
+        dinoMan -= -2;
+
+        Debug.Log("DinoVal = " + dinoMan);
+
+        Debug.Log(beebong);
+
+        Debug.Log(uhhhhh);
+
+        Debug.Log(nameIfYouWill);
+
+        total = dinoMan + mariririririooooo;
+
+        Debug.Log("total = " + total);
+
+        fullPhrase = uhhhhh + " " + theresMore;
+        Debug.Log(fullPhrase);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Assets/Week One/Scripts/Movementest.cs
@@ -0,0 +1,34 @@
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Movementest : MonoBehaviour
+{
+    public Vector3 startingPosition;
+
+    public Vector3 moveDirection;
+
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("He Appears");
+
+        //transforms the positino of the reference object to 0,0,0 --> (Vector 0)
+        this.transform.position = startingPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //deltatime is the time between frames, eg (30fps = 1/30th of a second & 60fps = 1/60th of a second)
+        // at 30fps you have 2 units per frame (60/30 = 2 ) at 60 fps you have 1 unit per frame (60/60 = 2) at 120fps. ypuo have 0.5 units per frame (60/120 = 0.5)
+        // deltatime takes the per-frame update to per-second instead
+
+        //moveDirection is currently (0,1,1
+        this.transform.position += moveDirection * speed * Time.deltaTime; //multiplying by Time.deltaTime means a "per second" instead of "per frame" Update
+
+        Debug.Log("Hes still here");
+    }
+}
